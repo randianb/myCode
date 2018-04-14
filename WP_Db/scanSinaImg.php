a2512c35cfbfaae3975ebbc721984c90105f4ee1
yhf <yhf7952@gmail.com>
2018-04-14 14:54:54 +0800

    备份网站所有图片

--- WP_Db/scanSinaImg.php
+++ WP_Db/scanSinaImg.php
@@ -1,19 +1,60 @@
 <?php
 require('db.php');
 
-$contents = $database->select("wp_posts", [
+
+$tbPosts = $table_prefix.'posts';
+$tbOption = $table_prefix.'options';
+$imgRegex = '/(http(s?)+:\/\/((?!yanzuz)\w|\.|\/|-|_)+?(jpg|jpeg|gif|png))"/i';
+$savePath = 'images/'; #图片存放目录，须777权限
+$count=0;
+
+$contents = $database->select($tbPosts, [
 	"post_content"
 ],[
-    //'LIMIT'=>[7,1]
+    'post_status'=>'publish'
 ]);
-
 foreach($contents as $con){
     //echo $con['post_content'];
-    preg_match('/(http[s]+:\/\/[^\s]*sinaimg[^\s]*)"/i', $con['post_content'], $matches);
+    preg_match_all($imgRegex, $con['post_content'], $matches);
 
     //print_r($matches);
-    foreach($matches as $key=>$img){
-        if($key >0)
-            echo $img.'<br/>';
+    foreach($matches[1] as $key=>$img){
+        if($key >0){
+            // echo $img.'<br/>';
+            // $count = $count+1;
+        }
     }
+}
+
+$contents = $database->select($tbOption, [
+	"option_value"
+],['option_name'=>'dux']);
+foreach($contents as $con){
+    preg_match_all($imgRegex, $con['option_value'], $matches);
+    
+    foreach($matches[1] as $key=>$img){
+        if($key >0 &&  $img){
+            //echo $img.'<br/>';
+            //$count = $count+1;
+            dlfile($img);
+        }
+    }
+}
+
+//echo $count;
+
+function dlfile($file_url)
+{
+    global $savePath;
+    $content = file_get_contents($file_url);
+    
+    $save_to = $savePath.preg_replace('/http(s?):\/\//','',$file_url);
+    $save_to_arr = explode("/",$save_to);
+    $save_to_path = str_replace(end($save_to_arr),'',$save_to);
+
+    if(!file_exists($save_to_path)){
+        mkdir($save_to_path,0777,true);
+    }
+    if(is_dir($save_to_path) && !file_exists($save_to))
+	    file_put_contents($save_to, $content);
 }
